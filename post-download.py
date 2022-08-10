422227ea1b4ffa7b4e6e1fb90b602f1705d2a8d1
Jason S <dragonrm7@gmail.com>
2022-08-09 21:11:09 -0400

    Update post-download.py
    Changes to split on getting the tor ID number. Also worked in unraring and detecting downloads with no directory.

--- post-download.py
+++ post-download.py
@@ -1,22 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/python3.6
+#####!/usr/bin/python3.6
 
 import os
+import sys
 import re
 import shutil
-import subprocess as sp 
+import subprocess as sp
 import distutils.core
+import patoolib
+import pyunpack
 #############################
 #
 #   Setup some folder info for later
 #   There are different paths for TV and Movies
-#   
-
+#
 #############################
-
-#   ************ Change the paths below to match your needs *****************
 destpath="/home/downloads/.process/"
-mvpath="/home/downloads/"
+mvpath="/home/movies/movies/"
 fullpath="/torrent-nzb/torrents/"
+#################################
+
+
+
 
 ## Intialize out lists
 torName=[]
@@ -39,17 +44,21 @@ output = output.split('\n')
 
 #### ------ Now to carve out what we need from each list item
 for i in output:
-    
+
     # we need to cut on the largest space block which should be the space between the Idle state and the Name
     carve1= i.split('         ')
-    #print(carve1)
+    #carve1=["The.Forgiven.2021.720p.WEB.H264-KBOX"]
+    print(carve1)
     ## we only need to keep item 2 from the carved up list item and append it to the new list
     torName.append(carve1[-1].strip())
 
     ## Lets get the ID for each torrent so we can remove it after our copy
-    carve2= i.split('   ')
-    #print(carve2[1])
+    carve2= i.split()
+    #carve2=["12"]
+    print(carve2[0])
     torID.append(carve2[0].strip())
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 ########################################################################
@@ -59,46 +68,66 @@ for i in output:
 for i,f in enumerate(torName):
 
     regexp = re.compile(r'[sS]+[0-9]+[eE]')
+    #this regex is used to see if the filename contains a Season number and Episode number
+    #If the filename does then it's handled like a TV show and sent to the sickchill drop directory
 
-    if regexp.search(f):
+    if regexp.search(f): #check for Season and Episode numbering
         showname=re.sub(r"^\s+","",f)
+        print("This is a TV show.          ")
         #print("cp -a \"",fullpath,showname,"\" -t ",destpath,sep='')
         ## combine the path with the directory name
         source_dir=fullpath+showname
         tvpath=destpath+showname
-        
+
         try:
+                            
+            print("Moving directory for processing")    
             distutils.dir_util.copy_tree(source_dir, tvpath)  #shutil.copytree(source_dir, destpath)
+            print("Directory move completed.")
         except:
             print("An exception occurred")
             print >> sys.stderr, "Could not copy folder"
             print >> sys.stderr, "Exception: %s" % str(e)
-            sys.exit(1)
-        
+            exit(1)
 
-    else:
+
+    else: #Season and Episode info not found so it's not a TV show, we can send it to the movie directory
         showname=re.sub(r"^\s+","",f)
         #print("cp -a \"",fullpath,showname,"\" -t ",destpath,sep='')
         ## combine the path with the directory name
         source_dir=fullpath+showname
         moviepath=mvpath+showname
         
+
         try:
-            distutils.dir_util.copy_tree(source_dir, moviepath)  #shutil.copytree(source_dir, destpath)
+            print("This is most likely a movie, let's see if there's any compressed files here")
+            for root,_,the_files in os.walk(source_dir):
+                print("Walking through ", source_dir)
+                for f in the_files:
+                    if f.lower().endswith(".rar"):
+                        decompfile=source_dir+"/"+f
+                        print("Found a rar file, gonna extract it.  ", f)
+                        patoolib.extract_archive(decompfile,outdir=mvpath)
+                        
+            #distutils.dir_util.copy_tree(source_dir, moviepath)  #shutil.copytree(source_dir, destpath)
 
         except:
-            print("An exception occurred") 
+            print("An exception occurred")
             print >> sys.stderr, "Could not copy folder"
             print >> sys.stderr, "Exception: %s" % str(e)
-            sys.exit(1)
+            exit(1)
+
+    #print("\nNow we can remove the torrents with the ID ----- ",torID[i],"\n\n")
+    #print("transmission-remote sickchill:9091 -t ",torID[i]," -rad",sep='')
+
+
 
     removetor="transmission-remote sickchill:9091 -t %s -rad"%(torID[i])
-
+    print(removetor)
     try:
         os.system(removetor)
     except:
         print("An exception occurred")
         print >> sys.stderr, "does not exist"
         print >> sys.stderr, "Exception: %s" % str(e)
-        sys.exit(1)
-
+        exit(1)
